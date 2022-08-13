70ec8fe1722b782c56269561d179907676df8f12
dibakar63 <110836157+dibakar63@users.noreply.github.com>
2022-08-14 01:12:06 +0530

    Add files via upload

---
+++ resumejson.js
@@ -0,0 +1,27 @@
+let Resume={
+    "name":"Dibakar Chandra Dey",
+    
+    education:
+    {
+    "primary" : "Purbasthali Nilmani Brahmachari Institution",
+    "college":"Techno India University, Kolkata,Salt-Lake",
+    },
+    internships:{
+        "intershala":"I have done internship in web developement in internshala",
+
+    },
+    certificates:{
+     geography:"I ranked 1st in geography merit exam in my state and and also perfored well in Math merit exam in my state" ,  
+    },
+    skills:{
+      name:"Python,Javascript,PHP,MySQL ,HTML,CSS"
+    },
+    languages:{
+    name:"english,bengali,hindi"
+    },
+    hobbies:"I have played as a professional football player in my early college day,I love to play cricket also,every morning running is  my habbit and also I love to  listening music and watching  movies",
+    contact:{
+    "mail":"dibakardey63@gmail.com",
+    "mobile":9635553429
+    },
+}
